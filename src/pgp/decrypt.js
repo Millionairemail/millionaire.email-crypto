6471d59477253fec0a8b4d47ab0352b8608f8eb4
Millionaire.Email <mithun@millionaire.email>
2025-07-14 19:02:40 +0530

    Update decrypt.js

--- src/pgp/decrypt.js
+++ src/pgp/decrypt.js
@@ -1,15 +1,15 @@
 import * as openpgp from 'openpgp';
 
-export async function pgpDecrypt(encryptedMessage, privateKeyArmored, passphrase = '') {
-  const privateKey = await openpgp.readPrivateKey({ armoredKey: privateKeyArmored });
-  await privateKey.decrypt(passphrase);
+export async function pgpDecrypt(encryptedMessage, armoredPrivateKey, passphrase) {
+  const privateKey = await openpgp.readPrivateKey({ armoredKey: armoredPrivateKey });
+  const decryptedKey = await openpgp.decryptKey({ privateKey, passphrase });
 
   const message = await openpgp.readMessage({ armoredMessage: encryptedMessage });
 
-  const { data: decrypted } = await openpgp.decrypt({
+  const { data } = await openpgp.decrypt({
     message,
-    decryptionKeys: privateKey
+    decryptionKeys: decryptedKey
   });
 
-  return decrypted;
+  return data; // string
 }
