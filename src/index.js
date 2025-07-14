c1888b123617c8c5bb2006164ba0888e7b70a928
Millionaire.Email <mithun@millionaire.email>
2025-07-14 12:53:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-const { encryptMessage } = require('./encrypt');
-const { decryptMessage } = require('./decrypt');
-const { signMessage } = require('./sign');
-const { verifySignature } = require('./verify');
-
-module.exports = {
-  encryptMessage,
-  decryptMessage,
-  signMessage,
-  verifySignature
-};
+export * from './crypto.js';
