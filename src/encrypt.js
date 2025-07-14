e14e01c8d02cb3e46d8d9e437d22c4849ae4001c
Millionaire.Email <mithun@millionaire.email>
2025-07-14 13:04:21 +0530

    Update and rename src/encrypt.js to src/pgp/encrypt.js

--- src/encrypt.js
+++
@@ -1,8 +0,0 @@
-export function encrypt(message, publicKey = 'FAKE_PUBLIC_KEY') {
-  const encrypted = Buffer.from(message).toString('base64');
-  return {
-    encrypted,
-    algorithm: 'AES-256 (simulated)',
-    publicKeyUsed: publicKey,
-  };
-}
