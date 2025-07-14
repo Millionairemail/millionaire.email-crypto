bc28a5cdf776fa7b9e65acbc59320a7c33145d0c
Millionaire.Email <mithun@millionaire.email>
2025-07-14 19:01:33 +0530

    Update verify.js

--- src/pgp/verify.js
+++ src/pgp/verify.js
@@ -1,14 +1,20 @@
 import * as openpgp from 'openpgp';
 
-export async function pgpVerify(signedMessage, publicKeyArmored) {
-  const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
+export async function pgpVerify(signedMessage, armoredPublicKey) {
+  const publicKey = await openpgp.readKey({ armoredKey: armoredPublicKey });
+
   const message = await openpgp.readCleartextMessage({ cleartextMessage: signedMessage });
 
-  const verification = await openpgp.verify({
+  const verificationResult = await openpgp.verify({
     message,
     verificationKeys: publicKey
   });
 
-  const { valid } = await verification.signatures[0].verified;
-  return { valid };
+  const { verified } = verificationResult.signatures[0];
+  try {
+    await verified;
+    return { valid: true };
+  } catch (e) {
+    return { valid: false };
+  }
 }
